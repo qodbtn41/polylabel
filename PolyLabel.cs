ea6aa0540476d47fa35b135ad663036f7dd1669e
qodbtn41 <qodbtn41@naver.com>
2020-10-12 13:35:30 +0900

    우선순위 큐로 처리
    누락된 코드가 있었다.
    그냥 큐가 아니라 우선순위 큐를 사용하도록 변경

--- PolyLabel.cs
+++ PolyLabel.cs
@@ -4,34 +4,113 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Priority_Queue;
 
 namespace PolyLabel
 {
     public class PolyLabel
     {
-        public static PolyLabel _polyLabel = null;
+        public PointF Centroid { get; set; }
+        public float Radius { get; set; }
 
-        public static PolyLabel GetPolyLabel()
+        public static PolyLabel GetPolyLabel(List<PointF> polygon, float precision = 1.0f, bool debug = false)
         {
-            if (_polyLabel == null)
-                _polyLabel = new PolyLabel();
+            float minX = float.MaxValue;
+            float minY = float.MaxValue;
+            float maxX = float.MinValue;
+            float maxY = float.MinValue;
 
-            return _polyLabel;
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                PointF p = polygon[i];
+                if (i == 0 || p.X < minX) minX = p.X;
+                if (i == 0 || p.Y < minY) minY = p.Y;
+                if (i == 0 || p.X > maxX) maxX = p.X;
+                if (i == 0 || p.Y > maxY) maxY = p.Y;
+            }
+
+            float width = maxX - minX;
+            float height = maxY - minY;
+            float cellSize = Math.Min(width, height);
+            float h = cellSize / 2;
+
+            SimplePriorityQueue<Cell> cellQueue = new SimplePriorityQueue<Cell>();
+
+            if (cellSize == 0)
+            {
+                PolyLabel degeneratePoleOfInaccessibility = new PolyLabel()
+                {
+                    Centroid = new PointF(minX, minY),
+                    Radius = 0
+                };
+                return degeneratePoleOfInaccessibility;
+            }
+
+            for (var x = minX; x < maxX; x += cellSize)
+            {
+                for (var y = minY; y < maxY; y += cellSize)
+                {
+                    Cell cell = new Cell(x + h, y + h, h, polygon);
+                    cellQueue.Enqueue(cell, cell.Max);
+                }
+            }
+
+            Cell bestCell = GetCentroidCell(polygon);
+
+            Cell bBoxCell = new Cell(minX + width / 2, minY + height / 2, 0, polygon);
+            if (bBoxCell.D > bestCell.D)
+                bestCell = bBoxCell;
+
+            int numProbes = cellQueue.Count;
+
+            while (cellQueue.Count != 0)
+            {
+                // pick the most promising cell from the queue
+                var cell = cellQueue.Dequeue();
+
+                // update the best cell if we found a better one
+                if (cell.D > bestCell.D)
+                {
+                    bestCell = cell;
+                    if (debug)
+                        Console.WriteLine("found best {0} after {1} probes", Math.Round(1e4 * cell.D) / 1e4, numProbes);
+                }
+
+                // do not drill down further if there's no chance of a better solution
+                if (cell.Max - bestCell.D <= precision)
+                    continue;
+
+                // split the cell into four cells
+                h = cell.H / 2;
+
+                Cell temp;
+                temp = new Cell(cell.X - h, cell.Y - h, h, polygon);
+                cellQueue.Enqueue(temp, temp.Max);
+                temp = new Cell(cell.X + h, cell.Y - h, h, polygon);
+                cellQueue.Enqueue(temp, temp.Max);
+                temp = new Cell(cell.X - h, cell.Y + h, h, polygon);
+                cellQueue.Enqueue(temp, temp.Max);
+                temp = new Cell(cell.X + h, cell.Y + h, h, polygon);
+                cellQueue.Enqueue(temp, temp.Max);
+                numProbes += 4;
+            }
+
+            if (debug)
+            {
+                Console.WriteLine("num probes: " + numProbes);
+                Console.WriteLine("best distance: " + bestCell.D);
+            }
+
+            PolyLabel poleOfInaccessibility = new PolyLabel()
+            {
+                Centroid = new PointF(bestCell.X, bestCell.Y),
+                Radius = bestCell.D
+            };
+
+            return poleOfInaccessibility;
         }
 
-        private PolyLabel()
-        {
-        }
-
-        Queue queue = new Queue();
-
-        private float CompareMax(Cell a, Cell b)
-        {
-            return b.max - a.max;
-        }
-
-        Cell GetCentroidCell(List<PointF> polygon)
+        private static Cell GetCentroidCell(List<PointF> polygon)
         {
             float area = 0;
             float x = 0;
@@ -53,94 +132,20 @@ namespace PolyLabel
             return new Cell(x / area, y / area, 0, polygon);
         }
 
-        public PointF GetPolyLabel(List<PointF> polygon, float precision = 1.0f, bool debug = false)
-        {
-            float minX = float.MaxValue;
-            float minY = float.MaxValue;
-            float maxX = float.MinValue;
-            float maxY = float.MinValue;
-
-            for (int i = 0; i < polygon.Count; i++)
-            {
-                PointF p = polygon[i];
-                if (i != 0 || p.X < minX) minX = p.X;
-                if (i != 0 || p.Y < minY) minY = p.Y;
-                if (i != 0 || p.X > maxX) maxX = p.X;
-                if (i != 0 || p.Y > maxY) maxY = p.Y;
-            }
-
-            float width = maxX - minX;
-            float height = maxY - minY;
-            float cellSize = Math.Min(width, height);
-            float h = cellSize / 2;
-
-            Queue<Cell> cellQueue = new Queue<Cell>();
-            //    var cellQueue = new Queue(null, compareMax);
-
-            if (cellSize == 0)
-                return new PointF(minX, minY);
-
-            for (var x = minX; x < maxX; x += cellSize)
-            {
-                for (var y = minY; y < maxY; y += cellSize)
-                {
-                    cellQueue.Enqueue(new Cell(x + h, y + h, h, polygon));
-                }
-            }
-
-            Cell bestCell = GetCentroidCell(polygon);
-
-            Cell bBoxCell = new Cell(minX + width / 2, minY + height / 2, 0, polygon);
-            if (bBoxCell.d > bestCell.d)
-                bestCell = bBoxCell;
-
-            int numProbes = cellQueue.Count;
-
-            while (cellQueue.Count != 0) {
-                // pick the most promising cell from the queue
-                var cell = cellQueue.Dequeue();
-
-                // update the best cell if we found a better one
-                if (cell.d > bestCell.d) {
-                    bestCell = cell;
-                    if (debug)
-                        Console.WriteLine("found best {0} after {1} probes", Math.Round(1e4 * cell.d) / 1e4, numProbes);
-                }
-
-                // do not drill down further if there's no chance of a better solution
-                if (cell.max - bestCell.d <= precision)
-                    continue;
-
-                // split the cell into four cells
-                h = cell.h / 2;
-                cellQueue.Enqueue(new Cell(cell.x - h, cell.y - h, h, polygon));
-                cellQueue.Enqueue(new Cell(cell.x + h, cell.y - h, h, polygon));
-                cellQueue.Enqueue(new Cell(cell.x - h, cell.y + h, h, polygon));
-                cellQueue.Enqueue(new Cell(cell.x + h, cell.y + h, h, polygon));
-                numProbes += 4;
-            }
-
-            if (debug)
-            {
-                Console.WriteLine("num probes: " + numProbes);
-                Console.WriteLine("best distance: " + bestCell.d);
-            }
-
-            return new PointF(bestCell.x, bestCell.y);
-        }
-
-
-
         class Cell
         {
-            public float x, y, h, d, max;
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float H { get; set; }
+            public float D { get; set; }
+            public float Max { get; set; }
             public Cell(float x, float y, float h, List<PointF> polygon)
             {
-                this.x = x;
-                this.y = y;
-                this.h = h;
-                this.d = PointToPolygonDist(x, y, polygon);
-                this.max = Convert.ToSingle(this.d + this.h * Math.Sqrt(2));
+                this.X = x;
+                this.Y = y;
+                this.H = h;
+                this.D = PointToPolygonDist(x, y, polygon);
+                this.Max = Convert.ToSingle(this.D + this.H * Math.Sqrt(2));
             }
 
             float PointToPolygonDist(float x, float y, List<PointF> polygon)
